f0808fcd11dd60a3dcf8b7ccc14dfac697c57b8c
dhanasekar2020 <dhanasekarsnit@gmail.com>
2020-05-31 07:01:37 +0530

    for heroku update

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/onlineshopping2020'),
+      dir: require('path').join(__dirname, './coverage/onlineshopangular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
